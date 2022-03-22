5c23269d89b6f415da038606b1081b6c2992bdb6
emirsahovic <emir.sahovic00@gmail.com>
2022-03-22 19:55:54 +0100

    Create profile and get all profiles endpoints

--- backend/controllers/profileController.js
+++ backend/controllers/profileController.js
@@ -1,9 +1,10 @@
 import asyncHandler from 'express-async-handler';
 import Profile from '../models/profileModel.js';
 import User from '../models/userModel.js';
+import normalizeUrl from 'normalize-url';
 
 // @desc    Get current users profile
-// @method  POST /api/profile/me
+// @method  GET /api/profile/me
 // @access  Private
 const getCurrentProfile = asyncHandler(async (req, res, next) => {
     const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'email']);
@@ -16,4 +17,57 @@ const getCurrentProfile = asyncHandler(async (req, res, next) => {
     res.status(200).json(profile);
 })
 
-export { getCurrentProfile }
+// @desc    Create profile
+// @method  POST /api/profile
+// @access  Private
+const createProfile = asyncHandler(async (req, res, next) => {
+    const {
+        website,
+        location,
+        bio,
+        skills,
+        facebook,
+        github,
+        linkedin
+    } = req.body;
+
+    if (!skills) {
+        res.status(400);
+        throw new Error('Please provide your skills');
+    }
+
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (website) profileFields.website = normalizeUrl(website);
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (skills) {
+        profileFields.skills = skills.split(',').map(skill => skill.trim());
+    }
+
+    profileFields.social = {};
+    if (facebook) profileFields.social.facebook = normalizeUrl(facebook);
+    if (github) profileFields.social.github = normalizeUrl(github);
+    if (linkedin) profileFields.social.linkedin = normalizeUrl(linkedin);
+
+    let profile = await Profile.findOne({ user: req.user.id });
+
+    if (profile) {
+        profile = await Profile.findOneAndUpdate({ user: req.user.id }, { $set: profileFields }, { new: true });
+        return res.status(200).json(profile);
+    }
+
+    profile = await Profile.create(profileFields);
+    res.status(201).json(profile);
+})
+
+// @desc    Get all profiles
+// @method  GET /api/profile
+// @access  Public
+const getAllProfiles = asyncHandler(async (req, res, next) => {
+    const profiles = await Profile.find().populate('user', ['name', 'email']);
+
+    res.status(200).json(profiles);
+})
+
+export { getCurrentProfile, createProfile, getAllProfiles }
--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { getCurrentProfile } from "../controllers/profileController.js";
+import { createProfile, getAllProfiles, getCurrentProfile } from "../controllers/profileController.js";
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.get('/me', protect, getCurrentProfile);
+router.post('/', protect, createProfile);
+router.get('/', getAllProfiles);
 
 export default router;
