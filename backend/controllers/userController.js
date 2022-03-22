40ee57afa8ac8d572b0fe211c28ab85bb8ec94a2
emirsahovic <emir.sahovic00@gmail.com>
2022-03-22 19:11:46 +0100

    Login and registration endpoints

---
+++ backend/controllers/userController.js
@@ -0,0 +1,70 @@
+import asyncHandler from 'express-async-handler';
+import bcrypt from 'bcryptjs';
+import User from '../models/userModel.js';
+
+// @desc    Register user
+// @method  POST /api/users/register
+// @access  Public
+const registerUser = asyncHandler(async (req, res, next) => {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+        res.status(400);
+        throw new Error('Please provide name, email and password');
+    }
+
+    const userMatch = await User.findOne({ email });
+
+    if (userMatch) {
+        res.status(400);
+        throw new Error('User already exists');
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email
+    });
+})
+
+// @desc    Login user
+// @method  POST /api/users/login
+// @access  Public
+const loginUser = asyncHandler(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        res.status(400);
+        throw new Error('Please provide email and password');
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        res.status(404);
+        throw new Error('Invalid credentials');
+    }
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400);
+        throw new Error('Invalid credentials');
+    }
+
+})
+
+export { registerUser, loginUser }
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,22 @@
 import express from "express";
 import dotenv from 'dotenv';
+dotenv.config();
 import colors from 'colors';
 import { connectDB } from './config/db.js';
-dotenv.config();
+import userRoutes from './routes/userRoutes.js';
+import { errorHandler } from './middleware/errorMiddleware.js';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 connectDB();
 
 const PORT = process.env.PORT || 5000;
+
+app.use('/api/users', userRoutes);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`.yellow.bold));
