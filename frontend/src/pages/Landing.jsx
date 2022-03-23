a9541bc42030e5c06e58171101ac3901e9aad8a7
emirsahovic <emir.sahovic00@gmail.com>
2022-03-23 15:33:10 +0100

    UI improvements

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -3,15 +3,18 @@ import { Link } from "react-router-dom"
 const Landing = () => {
     return (
         <div className="h-screen bg-image">
-            <div className="pt-56 text-center font-bold text-gray-300 xs:text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
-                Are you looking for <span className="text-green-500">employees</span>?
-                <div className="mt-3">
+            <div className="pt-56 text-center font-bold text-gray-300 xs:text-2xl sm:text-3xl md:text-4xl lg:text-5xl" style={{ lineHeight: '1.2' }}>
+                Are you looking for <span className="text-green-500">employees</span> or <br /> do you want to become one of them?
+                <div>
                     <span className="text-green-500">Jobstify</span> will help you.
                 </div>
             </div>
-            <div className="flex flex-col justify-center items-center mt-8">
+            <div className="flex justify-center items-center mt-8 space-x-5">
                 <Link to='/profiles' className="px-6 py-2 bg-green-800 text-gray-300 font-bold rounded text-lg hover:opacity-80 transition duration-150 active:-translate-y-2">
                     View Candidates
+                </Link>
+                <Link to='/login' className="px-6 py-2 bg-green-800 text-gray-300 font-bold rounded text-lg hover:opacity-80 transition duration-150 active:-translate-y-2">
+                    Log in to become a candidate
                 </Link>
             </div>
         </div>
