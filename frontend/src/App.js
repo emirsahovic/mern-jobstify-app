1e5c893549dc867bea8fd780863507058b187181
emirsahovic <emir.sahovic00@gmail.com>
2022-03-23 15:36:11 +0100

    Landing page route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Landing from "./pages/Landing";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={<Landing />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
     </Router>
   );
