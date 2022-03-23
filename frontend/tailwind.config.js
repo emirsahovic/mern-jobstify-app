53910e5c7e829ae4b8a4fa24b96138f1fb70c83b
emirsahovic <emir.sahovic00@gmail.com>
2022-03-23 14:53:39 +0100

    Breakpoint modification

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,6 +4,25 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      'xs': '350px',
+      // => @media (min-width: 390px) { ... }
+
+      'sm': '500px',
+      // => @media (min-width: 500px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
   },
   plugins: [],
 }
