4bda614acf5dae9cf01909ee4f4b860cf2d95fcb
emirsahovic <emir.sahovic00@gmail.com>
2022-03-23 11:50:43 +0100

    Title added

--- backend/models/postModel.js
+++ backend/models/postModel.js
@@ -5,6 +5,10 @@ const postSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
     },
+    title: {
+        type: String,
+        required: true
+    },
     text: {
         type: String,
         required: true
