0e4c12ac8aee5685881fc53cdb792b55b3862229
emirsahovic <emir.sahovic00@gmail.com>
2022-03-23 22:54:02 +0100

    Loading spinner

---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+import spinner from '../assets/spinner.gif';
+
+const Spinner = () => {
+    return (
+        <img className='mx-auto block w-96 mt-4' src={spinner} alt="Loading..." />
+    )
+}
+
+export default Spinner;
