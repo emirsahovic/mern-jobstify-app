7ed847b1dac657a35df3aa302f7ca306d4aff08e
emirsahovic <emir.sahovic00@gmail.com>
2022-03-26 19:11:51 +0100

    UI improvements

--- frontend/src/components/ProfileEducation.jsx
+++ frontend/src/components/ProfileEducation.jsx
@@ -1,11 +1,17 @@
 import Moment from 'react-moment';
+import { MdOutlineSchool, MdDateRange } from 'react-icons/md';
 
 const ProfileEducation = ({ edu: { school, degree, fieldofstudy, to, from } }) => {
     return (
-        <div className='bg-green-300 shadow-xl border-2 border-gray-300 rounded-lg w-full p-6 hover:-translate-y-5 hover:scale-110 hover:border-green-300 transition duration-500' style={{ lineHeight: 2 }}>
-            <h3 className="font-bold text-2xl mb-2">{school}</h3>
-            <p className='text-gray-500'><Moment format='MM/YYYY'>{from}</Moment> - {!to ? ' Now' : <Moment format='MM/YYYY'>{to}</Moment>}</p>
-            <p className='text-lg'>
+        <div className='bg-green-800 text-white shadow-xl border-2 border-gray-300 rounded-lg w-full p-6 hover:-translate-y-5 hover:scale-110 hover:border-green-300 transition duration-500' style={{ lineHeight: 2 }}>
+            <div className='flex items-center'>
+                <h3 className="font-bold text-2xl mb-2">{school}</h3>
+                <MdOutlineSchool className='text-3xl ml-2 mb-1' />
+            </div>
+            <div className='flex items-center my-1'>
+                <MdDateRange className='text-2xl mr-2' />
+                <p><Moment format='MM/YYYY'>{from}</Moment> - {!to ? ' Now' : <Moment format='MM/YYYY'>{to}</Moment>}</p>
+            </div>            <p className='text-lg'>
                 <strong>Degree: </strong> {degree}
             </p>
             <p className='text-lg'>
--- frontend/src/components/ProfileExperience.jsx
+++ frontend/src/components/ProfileExperience.jsx
@@ -1,10 +1,18 @@
 import Moment from 'react-moment';
+import { FaRegBuilding } from 'react-icons/fa';
+import { MdDateRange } from 'react-icons/md';
 
 const ProfileExperience = ({ exp: { company, from, to, title, description } }) => {
     return (
         <div className='bg-gray-700 text-white shadow-xl border-2 border-gray-300 rounded-lg md:w-full p-6 hover:-translate-y-5 hover:scale-110 hover:border-green-300 transition duration-500' style={{ lineHeight: 2 }}>
-            <h3 className="font-bold text-2xl mb-2">{company}</h3>
-            <p className='text-gray-200'><Moment format='MM/YYYY'>{from}</Moment> - {!to ? ' Now' : <Moment format='MM/YYYY'>{to}</Moment>}</p>
+            <div className='flex items-center'>
+                <h3 className="font-bold text-2xl mb-2">{company}</h3>
+                <span><FaRegBuilding className='text-3xl mb-2 ml-2' /></span>
+            </div>
+            <div className='flex items-center my-1'>
+                <MdDateRange className='text-2xl mr-2 text-gray-200' />
+                <p className='text-gray-200'><Moment format='MM/YYYY'>{from}</Moment> - {!to ? ' Now' : <Moment format='MM/YYYY'>{to}</Moment>}</p>
+            </div>
             <p className='text-lg'>
                 <strong>Position: </strong> {title}
             </p>
